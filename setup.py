b5520c76764b5b017e3dc1b41185cb79ecc973a3
sharma-srishti <srishti16702@gmail.com>
2026-04-19 18:23:43 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Srishti",
+    author_email="srishti16702@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
